d8f64170daef5f5ee73b04900af5f92196449d4b
庞龙 <henrypang@anjuke.com>
2013-12-25 14:38:29 +0800

    touch top item v1.0

---
+++ freePos.js
@@ -0,0 +1,30 @@
+function FreePos (opts) {
+    function move() {
+        scrollTop = document.documentElement.scrollTop | document.body.scrollTop;
+        orgTop = objAb.offset().top - scrollTop;
+        orgLeft = objAb.offset().left;
+        if ($.browser.version == '6.0') {
+            0 > (+orgTop) ? objFx.show().css({position:'absolute',top:scrollTop,left:(+orgLeft)+'px'}) : objFx.hide();
+        } else {
+            0 > (+orgTop) ? objFx.show().css({position:'fixed',top:'0px',left:(+orgLeft)+'px'}) : objFx.hide();
+        }
+    }
+    var objAb = opts.objAb,
+        objFx = opts.objFx,
+        scrollTop = 0,
+        orgTop = 0,
+        orgLeft = 0;
+    $(window).resize(function() {
+        move();
+    });
+    $(window).scroll(function() {
+        move();
+    });
+}
+ 
+$(function() {
+    var g = new FreePos({
+        objAb : $('#bar4'),
+        objFx : $('#bar3')
+    });
+});
